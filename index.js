37ddcbf0d3cfb2ba3e7d650596aafcf172985a3d
Roman Sandulskyi <emixever@gmail.com>
2018-03-22 11:04:55 +0200

    server

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api')
+const http = require('http')
 
 const BOT_TOKEN = '534912162:AAH3xupuDJEhetwqZatv9o3w2Q2T8H3u1eA'
+
+http.createServer.listen(process.env.PORT || 5000).on('request', (req, res) => {
+    res.end('')
+})
 
 const bot = new TelegramBot(BOT_TOKEN, {polling: true})
 
