81ccc6b90761f4d8d682fc9f408f1ea06729a075
Roman Sandulskyi <emixever@gmail.com>
2018-03-22 11:44:11 +0200

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api')
 const http = require('http')
 
-const BOT_TOKEN = '534912162:AAH3xupuDJEhetwqZatv9o3w2Q2T8H3u1eA'
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
 http.createServer().listen(process.env.PORT || 5000).on('request', (req, res) => {
     res.end('')
@@ -10,6 +10,6 @@ http.createServer().listen(process.env.PORT || 5000).on('request', (req, res) =>
 const bot = new TelegramBot(BOT_TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    console.log(msg.text)
+    console.log(`${msg.from.first_name}: ${msg.text}`)
     bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}`)
 })
